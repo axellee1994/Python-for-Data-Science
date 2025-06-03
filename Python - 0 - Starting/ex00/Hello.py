b3428157bee4a157db384177ecd183b1cb3cf2ea
Axel Lee <axellee1994@gmail.com>
2025-06-03 11:01:44 +0800

    Completed ex00

---
+++ Python - 0 - Starting/ex00/Hello.py
@@ -0,0 +1,27 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+print(type(ft_list))    # Ordered, changeable and allow duplicates
+print(type(ft_tuple))   # Ordered, unchangeable and allow duplicates
+print(type(ft_set))     # Unordered, unchangeable, and no duplicates
+print(type(ft_dict))    # Ordered, changeable and no duplicates
+
+#Converting tuple(immutable) to list(mutable) and modifying it
+ft_tuple_convert = list(ft_tuple)
+ft_tuple_convert[1] = "Singapore"
+ft_tuple = tuple(ft_tuple_convert)
+
+#Modifying set value
+ft_set.remove("tutu!")
+ft_set.add("Singapore")
+
+#Modifying dictionary value
+ft_dict.update({"Hello": "42Singapore"})
+
+#your code here
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
